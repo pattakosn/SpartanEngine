116bc791a633ac884e9597ea24a2a98b9e88bba0
N. Pattakos <pattakosn@fastmail.com>
2023-06-22 00:15:32 +0200

    order if included headers was breaking enum class declarations in RHI_Definitions.h

--- runtime/Core/Window.cpp
+++ runtime/Core/Window.cpp
@@ -22,11 +22,11 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //= INCLUDES =====================
 #include "pch.h"
 #include "Window.h"
-#include "SDL2/SDL.h"
-#include "SDL2/SDL_syswm.h"
 #include "../Input/Input.h"
 #include "../Display/Display.h"
 #include "../Rendering/Renderer.h"
+#include "SDL2/SDL.h"
+#include "SDL2/SDL_syswm.h"
 //================================
 
 //= LINKING ============================
