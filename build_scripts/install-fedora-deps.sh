bf897836c144e14d32a1518ba983921e8ad1b8d9
N. Pattakos <pattakosn@fastmail.com>
2023-06-23 00:13:48 +0200

    update fedora deps list

--- build_scripts/install-fedora-deps.sh
+++ build_scripts/install-fedora-deps.sh
@@ -1,4 +1,4 @@
-dnf install premake assimp-devel bullet-devel freeimage-devel SDL2-devel freetype-devel spirv-tools-devel vulkan vulkan-tools pugixml-devel
+dnf install premake assimp-devel bullet-devel freeimage-devel SDL2-devel freetype-devel spirv-tools-devel vulkan vulkan-tools pugixml-devel vulkan-loader-devel
 
 compressonator gpuopen https://github.com/GPUOpen-Tools/Compressonator/releases
 fmod
