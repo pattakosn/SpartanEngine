e3b681a4fd344fa839ee706ea8a07af1a95ae1a8
N. Pattakos <pattakosn@fastmail.com>
2025-03-09 01:19:46 +0100

    compiler warning: del extra parameter

--- editor/Widgets/Properties.cpp
+++ editor/Widgets/Properties.cpp
@@ -483,7 +483,7 @@ void Properties::ShowRenderable(spartan::Renderable* renderable) const
 
         ImGui::Text("Visible");
         ImGui::SameLine(column_pos_x);
-        ImGui::LabelText("##renderable_visible", is_visible ? "true" : "false", ImGuiInputTextFlags_AutoSelectAll | ImGuiInputTextFlags_ReadOnly);
+        ImGui::LabelText("##renderable_visible", is_visible ? "true" : "false");
 
         //= MAP =========================================================
         renderable->SetFlag(RenderableFlags::CastsShadows, cast_shadows);
